cb00e52ce1f5cf1c443d272261e825159dc2a18f
XDUyhfu <yhfu@stu.xidian.edu.cn>
2023-01-18 17:10:52 +0800

    feat: 优化初始值逻辑

--- packages/Re-Gen-Hooks/src/hooks/index.ts
+++ packages/Re-Gen-Hooks/src/hooks/index.ts
@@ -23,6 +23,7 @@ interface IResultAtoms<T = unknown> {
 
 export const useAtoms = (AtomInOut:IAtomInOut, RelationConfig: IConfigItem[] ) => {
     const names = RelationConfig.map( item => item.name );
+    const getConfigItem = (name: string) => RelationConfig.filter(item => item.name === name)[0];
     
     const Atoms:IResultAtoms = names.reduce( ( pre, name ) => {
         const inout$ = AtomInOut( name );        
@@ -30,7 +31,7 @@ export const useAtoms = (AtomInOut:IAtomInOut, RelationConfig: IConfigItem[] ) =
         return {
             ...pre,
             [`${ name }In$`]: inout$[`${ name }In$`],
-            [`${name}Value`]: useObservable(() => inout$[`${ name }Out$`])
+            [`${name}Value`]: useObservable(() => inout$[`${ name }Out$`], getConfigItem(name).init)
         };
     }, {} as IResultAtoms );
 

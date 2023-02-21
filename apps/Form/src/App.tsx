4cd954fa34b7390bb875e5208193a1ea676956e4
XDUyhfu <yhfu@stu.xidian.edu.cn>
2023-02-21 15:50:53 +0800

    feat: 添加case

--- apps/Form/src/App.tsx
+++ apps/Form/src/App.tsx
@@ -1,97 +1,27 @@
-import React, { useState } from "react";
-import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Select, Space } from "antd";
-import { Depend_handle, Handle } from "./config";
+import { ReGen } from "@yhfu/re-gen";
+import React from "react";
+import { ConfigItems } from "./config";
+import { useAtomsValue ,useAtomsCallback } from "@yhfu/re-gen-hooks";
+import { Button } from "antd";
+
+
+const AtomInOut = ReGen( "FORM_CACHE_KEY", ConfigItems );
 
 const App: React.FC = () => {
-  const [form] = Form.useForm();
+  const { Items, ItemNames } = useAtomsValue( AtomInOut, ConfigItems );
+  const { addItemCallback } = useAtomsCallback( AtomInOut, ConfigItems );
 
-  const onFinish = ( values: any ) => {
-    console.log( "Received values of form:", values );
-  };
+  return <div>
 
-  const [dependNamesOptions, setDependNamesOptions] = useState( [{ label: "123", value: "123" }] );
+    { JSON.stringify( Items ) }
+    <br />
+    { JSON.stringify( ItemNames ) }
+    <br />
 
-  return (
-    <div >
-      <Form
-        form={ form }
-        name="dynamic_form_complex"
-        onFinish={ onFinish }
-      >
-        <Form.List name="ConfigList">
-          { ( fields, { add, remove } ) => (
-            <>
-              { fields.map( ( field ) => (
-                <Space key={ field.key } align="baseline">
-                  <Form.Item
-                    { ...field }
-                    label="name"
-                    name={ [field.name, "name"] }
-                    rules={ [{ required: true, message: "需要一个唯一的name" }] }
 
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item
-                    { ...field }
-                    label="init"
-                    name={ [field.name, "init"] }
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item
-                    { ...field }
-                    label="handle"
-                    name={ [field.name, "handle"] }
-                  >
-                    <Input.TextArea rows={ 4 } defaultValue={ Handle } />
-                  </Form.Item>
-                  <Form.Item
-                    { ...field }
-                    label="depend_names"
-                    name={ [field.name, "depend_names"] }
-                    shouldUpdate={ ( prevValues, curValues ) => {
-                      console.log( prevValues, curValues );
-                      return true;
-                    }
-                    }
-                  >
-                    {
-                      ( val ) => {
-                        console.log( val );
-                        return <Select mode="multiple" options={ dependNamesOptions } style={ { width: 200 } } />;
-                      }
-                    }
+    <Button onClick={addItemCallback}>添加</Button>
 
-                  </Form.Item>
-                  <Form.Item
-                    { ...field }
-                    label="depend_handle"
-                    name={ [field.name, "depend_handle"] }
-                  >
-                    <Input.TextArea rows={ 4 } defaultValue={ Depend_handle } />
-                  </Form.Item>
-                  <MinusCircleOutlined onClick={ () => remove( field.name ) } />
-                </Space>
-              ) ) }
-
-              <Form.Item>
-                <Button type="dashed" onClick={ () => add() } block icon={ <PlusOutlined /> }>
-                  添加状态
-                </Button>
-              </Form.Item>
-            </>
-          ) }
-        </Form.List>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            确认
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+  </div>;
 };
 
 export default App;
--- apps/Form/src/config.ts
+++ apps/Form/src/config.ts
@@ -1,2 +1,44 @@
-export const Depend_handle = "async ( [name1, name2, ...names] ) => val";
-export const Handle = "async ( val ) => val";
+import { IConfigItem } from "@yhfu/re-gen";
+
+interface IItem {
+    id: string;
+    name: string;
+    init?: any;
+    handle?: string; // function string类型
+    dependNames?: string[];
+    dependHandle?: ( val: string[] ) => any;
+}
+
+
+export const ConfigItems: IConfigItem[] = [
+    {
+        name: "Items",
+        init: [],
+        depend: {
+            names: ["addItem"],
+            handle: ( [Items, addItem] ): IItem[] => {
+                console.log( "Items", Items );
+                console.log( "addItem", addItem );
+                return [
+                    ...Items,
+                    addItem
+                ];
+            }
+        }
+    },
+    {
+        name: "addItem",
+        handle (): IItem {
+            return { id: Date.now().toString(), name: Date.now().toString() };
+        }
+    },
+    {
+        name: "ItemNames",
+        init: [],
+        depend: {
+            names: ["Items"],
+            handle: ( [ItemNames, Items]: [ItemName: string[], Items: IItem[]] ) => Items.map( item => item.name )
+        }
+    },
+
+];  

b3af1facf0b799331e852b859e042ca135988e05
XDUyhfu <yhfu@stu.xidian.edu.cn>
2023-03-06 15:13:34 +0800

    add: auto-complete

--- apps/AutoComplete/src/Menu/menuItem.tsx
+++ apps/AutoComplete/src/Menu/menuItem.tsx
@@ -1,40 +1,40 @@
 import classNames from "classnames";
-import Menu, { MenuContext } from "./menu";
-import { useContext } from "react";
+import { MenuContext } from "./menu";
+import React, { useContext } from "react";
 
 export interface MenuItemProps {
-    index: number;
-    disabled?: boolean;
-    className?: string;
-    style?: React.CSSProperties;
-    children?: React.ReactNode;
+	index: number;
+	disabled?: boolean;
+	className?: string;
+	style?: React.CSSProperties;
+	children?: React.ReactNode;
 }
 
 const MenuItem: React.FC<MenuItemProps> = ( {
-    index,
-    disabled,
-    className,
-    style,
-    children
+	index,
+	disabled,
+	className,
+	style,
+	children
 } ) => {
-    const context = useContext( MenuContext );
+	const context = useContext( MenuContext );
 
-    const classes = classNames( "menu-item", className, {
-        "is-disabled": disabled,
-        "is-active": context.index === index
-    } );
+	const classes = classNames( "menu-item", className, {
+		"is-disabled": disabled,
+		"is-active": context.index === index
+	} );
 
-    const handleClick = () => {
-        if ( context.onSelect && !disabled ) {
-            context.onSelect( index );
-        }
-    };
+	const handleClick = () => {
+		if ( context.onSelect && !disabled ) {
+			context.onSelect( index );
+		}
+	};
 
 
-    return <li className={ `${ classes } px-4 cursor-pointer hover:bg-gray-400 active:bg-gray-700 select-none` }
-               style={ style } onClick={ handleClick }>
-        { children }
-    </li>;
+	return <li className={ `${ classes } px-4 cursor-pointer hover:bg-gray-400 active:bg-gray-700 select-none` }
+	           style={ style } onClick={ handleClick }>
+		{ children }
+	</li>;
 };
 
 export default MenuItem;

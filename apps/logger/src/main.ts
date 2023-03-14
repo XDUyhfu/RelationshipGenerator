1297faeb6b53341c6f94f45ce2ea4cfbb521f69a
XDUyhfu <yhfu@stu.xidian.edu.cn>
2023-03-14 21:58:01 +0800

    fix: 修复已知问题

--- apps/logger/src/main.ts
+++ apps/logger/src/main.ts
@@ -1,12 +1,12 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import { setupCounter } from "./counter";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
+    <a href="https://vitejs.dev" target="_blank">  
+      <img src="/vite.svg" class="logo" alt="Vite logo" /> 
+    </a> 
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
@@ -18,6 +18,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);

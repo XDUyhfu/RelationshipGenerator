d98d69f6d623df8437620b614557553169819c78
XDUyhfu <yhfu@stu.xidian.edu.cn>
2023-07-13 16:27:07 +0800

    feat: 发包

---
+++ packages/Re-Component/src/components/re-array-field/index.tsx
@@ -0,0 +1,20 @@
+import { cloneElement } from "react";
+// import { IReArrayField } from "../../type";
+// import { useArrayItemValue, useRestProps } from "../../hook";
+//
+// export const ReArrayField = (props: IReArrayField) => {
+//     const { name, children, arrName = "", lineKey = 0, ...rest } = props;
+//
+//     const value = useArrayItemValue(arrName, lineKey, name);
+//
+//     const restProps = useRestProps(rest, children);
+//
+//     return (
+//         <span>
+//             {cloneElement(children, {
+//                 value,
+//                 ...restProps,
+//             })}
+//         </span>
+//     );
+// };

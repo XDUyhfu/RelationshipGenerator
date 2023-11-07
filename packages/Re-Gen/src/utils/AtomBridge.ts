543fd1b21fac31598eb144bf34beed00ebcad758
XDUyhfu <yhfu@stu.xidian.edu.cn>
2023-11-08 00:31:01 +0800

    feat: 新增atom bridge

---
+++ packages/Re-Gen/src/utils/AtomBridge.ts
@@ -0,0 +1,33 @@
+import type { Observable } from "rxjs";
+import { ReplaySubject } from "rxjs";
+
+export class AtomBridge {
+    private bridge = new Map<string, ReplaySubject<any>[]>();
+    private static instance: null | AtomBridge = null;
+
+    private constructor() {}
+
+    static getInstance() {
+        if (this.instance) return this.instance;
+        this.instance = new AtomBridge();
+        return this.instance;
+    }
+
+    generate(key: string) {
+        const observable = new ReplaySubject(0);
+        this.bridge.set(key, [...(this.bridge.get(key) ?? []), observable]);
+        return observable;
+    }
+
+    /**
+     * 统一去订阅一个 observable
+     */
+    subscribe(key: string, observable: Observable<any>) {
+        this.bridge.has(key) &&
+            this.bridge.get(key)!.forEach((o) => observable.subscribe(o));
+    }
+
+    getBridge(key: string) {
+        return this.bridge.get(key);
+    }
+}
